2c860fb43b33ec2ce116badb87e7dc9292efb786
Elliot Farmer Garcia <epfarmer@CougarNet.UH.EDU>
2019-11-12 19:20:50 -0600

    master

--- ConsoleApplication1/ConsoleApplication1/Farmer_Garcia_Elliot_H7.cpp
+++ ConsoleApplication1/ConsoleApplication1/Farmer_Garcia_Elliot_H7.cpp
@@ -14,6 +14,17 @@ Operating System    : Windows 7
 /*
 Problem Statement
 
+This program allows the user to compare their desktop or laptop computer to
+an Apple desktop or laptop computer. The manufacturer, model, and year are
+compared for desktops, and the manufacturer, model, and weight are compared
+for laptops.
+
+Questions:
+1. Program model?
+2. D1 and L1 - overloaded constructors or mutators?
+3. destructors - blank?
+4. accessors with return type void - still accessors?
+5. displayComputer() - member function of class Computer y/n?
 
 */
 
@@ -22,6 +33,7 @@ Problem Statement
 
 using namespace std;
 
+//base class Computer declaration
 class Computer {
 
 	string manufacturer;
@@ -33,10 +45,13 @@ public:
 	Computer(string ma, string mo);
 	~Computer();
 
+    void setManufacturer(string ma);
+    void setModel(string mo);
 	void displayComputer(void);
 
 };
 
+//derived class Desktop declaration, inherits Computer
 class Desktop : public Computer {
 
 	int year;
@@ -47,10 +62,12 @@ public:
 	Desktop(string ma, string mo, int y);
 	~Desktop();
 
+    void setYear(int y);
 	void displayDesktop(void);
 
 };
 
+//derived class Laptop declaration, inherits Computer
 class Laptop : public Computer {
 
 	float weight;
@@ -61,21 +78,90 @@ public:
 	Laptop(string ma, string mo, float w);
 	~Laptop();
 
+    void setWeight(float w);
 	void displayLaptop(void);
 
 };
 
+//creates instances of Desktop and Laptop with default values, as well as
+//instances of Desktop and Laptop with user-set values, allowing the user to
+//visually compare them
 int main() {
 
+    cout << "This program allows you to visually compare stats for "
+        "your computer against\nApple products.\n\n";
+
+    //default Desktop
+    Desktop D1;
+
+    //set D1 to 2019 Apple Mac Pro using mutator functions
+    D1.setManufacturer("Apple");
+    D1.setModel("Mac Pro");
+    D1.setYear(2019);
+
+    //default Laptop
+    Laptop L1;
+
+    //set L1 to Apple MacBook Pro weighing 4.0lbs using mutator functions
+    L1.setManufacturer("Apple");
+    L1.setModel("MacBook Pro");
+    L1.setWeight(4.0);
+
+    //values for custom Desktop and Laptop
+    string D2ma, D2mo, L2ma, L2mo;
+    int D2y;
+    float L2w;
+
+    //user set custom Desktop values
+    cout << "Please enter desktop manufacturer: ";
+    cin >> D2ma;
+    cout << "Please enter desktop model: ";
+    cin >> D2mo;
+    cout << "Please enter desktop year: ";
+    cin >> D2y;
+    cout << "\n";
+
+    //custom Desktop is created with overloaded constructor
+    Desktop D2(D2ma, D2mo, D2y);
+ 
+    //user set custom Laptop values
+    cout << "Please enter laptop manufacturer: ";
+    cin >> L2ma;
+    cout << "Please enter laptop model: ";
+    cin >> L2mo;
+    cout << "Please enter laptop weight: ";
+    cin >> L2w;
+    cout << "\n";
+
+    //custom Laptop is created with overloaded constructor
+    Laptop L2(L2ma, L2mo, L2w);
+
+    //prints values for default and custom laptops and desktops, allowing
+    //user to visually compare them
+    D1.displayDesktop();
+    cout << "vs.\n";
+    D2.displayDesktop();
+
+    cout << "\n";
+
+    L1.displayLaptop();
+    cout << "vs.\n";
+    L2.displayLaptop();
+
+    return 0;
+
 }
 
+
+//default Computer constructor
 Computer::Computer() {
 
-	manufacturer = "Apple";
-	model = "iComputer";
+	manufacturer = "";
+	model = "";
 
 }
 
+//overloaded Computer constructor, sets manufacturer and model to parameters
 Computer::Computer(string ma, string mo) {
 
 	manufacturer = ma;
@@ -83,50 +169,98 @@ Computer::Computer(string ma, string mo) {
 
 }
 
+//Computer destructor
 Computer::~Computer() {
 
 }
 
+//mutator that sets computer manufacturer
+void Computer::setManufacturer(string ma) {
+
+    manufacturer = ma;
+
+}
+
+//mutator that sets computer model
+void Computer::setModel(string mo) {
+
+    model = mo;
+
+}
+
+//accessor function - prints manufacturer and model of Computer
 void Computer::displayComputer() {
 
 	cout << manufacturer << " " << model;
 
 }
 
-Desktop::Desktop() {
+//default Desktop constructor
+Desktop::Desktop() : Computer() {
 
-	year = 2019;
+	year = 0;
 
 }
 
+//overloaded Desktop constructor, allows user to set custom manufacturer,
+//model, and year
 Desktop::Desktop(string ma, string mo, int y) : Computer(ma, mo) {
 
 	year = y;
 
 }
 
+//Desktop destructor
+Desktop::~Desktop() {
+
+}
+
+//mutator function, sets Desktop year
+void Desktop::setYear(int y) {
+
+    year = y;
+
+}
+
+//accessor function - prints manufacturer, model, and year of Desktop
 void Desktop::displayDesktop(void) {
 
 	Computer::displayComputer();
-	cout << " (" << year << ")\n";
+	cout << " (" << year << " edition)\n";
 
 }
 
-Laptop::Laptop() {
+//default Laptop constructor
+Laptop::Laptop() : Computer() {
 	
-	weight = 1.1;
+	weight = 0.0f;
 
 }
 
+//overloaded Laptop constructor, allows user to set custom manufacturer,
+//model, and weight
 Laptop::Laptop(string ma, string mo, float w) : Computer(ma, mo) {
 
 	weight = w;
 
 }
 
+//Laptop destructor
+Laptop::~Laptop() {
+
+}
+
+//Laptop mutator function, sets weight
+void Laptop::setWeight(float w) {
+    
+    weight = w;
+
+}
+
+//accessor function - prints Laptop manufacturer, model, and weight
 void Laptop::displayLaptop(void) {
 
 	Computer::displayComputer();
-	cout << "(" << weight << " lbs.)\n";
+	cout << " (" << weight << " lbs.)\n";
 
 }
